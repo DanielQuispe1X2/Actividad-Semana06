9517541fabeb6c5c83881e58e65ea294b08ae3d2
DanielQuispe1X2 <85980908+DanielQuispe1X2@users.noreply.github.com>
2025-11-13 20:40:04 -0500

    Update generative model version to 2.5-pro

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ st.markdown("""
 # ==============================
 genai.configure(api_key="AIzaSyA0oGgigHTC3EqaGBTTro62yUFrVWoS2J0")
 
-modelo = genai.GenerativeModel("gemini-1.5-flash")
+modelo = genai.GenerativeModel("gemini-2.5-pro")
 
 def procesar_pedido(texto):
     prompt = f"""
